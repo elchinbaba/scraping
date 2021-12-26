d01eb16b3eb83aee0e6cd8cc4c6d06d93f4e2165
Elçin Babalı <babalielchin@gmail.com>
2021-12-26 06:20:43 +0400

    added labels

--- scraping/findjob.Designer.cs
+++ scraping/findjob.Designer.cs
@@ -34,6 +34,9 @@ namespace scraping
             this.remote_or_office = new System.Windows.Forms.ComboBox();
             this.jobs = new System.Windows.Forms.RichTextBox();
             this.get = new System.Windows.Forms.Button();
+            this.job_title_lbl = new System.Windows.Forms.Label();
+            this.work_experience_lbl = new System.Windows.Forms.Label();
+            this.remote_office_lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // job_title
@@ -72,7 +75,7 @@ namespace scraping
             // get
             // 
             this.get.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.get.Location = new System.Drawing.Point(332, 114);
+            this.get.Location = new System.Drawing.Point(336, 114);
             this.get.Name = "get";
             this.get.Size = new System.Drawing.Size(99, 43);
             this.get.TabIndex = 4;
@@ -80,11 +83,44 @@ namespace scraping
             this.get.UseVisualStyleBackColor = true;
             this.get.Click += new System.EventHandler(this.get_Click);
             // 
+            // job_title_lbl
+            // 
+            this.job_title_lbl.AutoSize = true;
+            this.job_title_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.job_title_lbl.Location = new System.Drawing.Point(88, 37);
+            this.job_title_lbl.Name = "job_title_lbl";
+            this.job_title_lbl.Size = new System.Drawing.Size(64, 20);
+            this.job_title_lbl.TabIndex = 5;
+            this.job_title_lbl.Text = "Job title";
+            // 
+            // work_experience_lbl
+            // 
+            this.work_experience_lbl.AutoSize = true;
+            this.work_experience_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.work_experience_lbl.Location = new System.Drawing.Point(319, 37);
+            this.work_experience_lbl.Name = "work_experience_lbl";
+            this.work_experience_lbl.Size = new System.Drawing.Size(127, 20);
+            this.work_experience_lbl.TabIndex = 6;
+            this.work_experience_lbl.Text = "Work experience";
+            // 
+            // remote_office_lbl
+            // 
+            this.remote_office_lbl.AutoSize = true;
+            this.remote_office_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.remote_office_lbl.Location = new System.Drawing.Point(600, 37);
+            this.remote_office_lbl.Name = "remote_office_lbl";
+            this.remote_office_lbl.Size = new System.Drawing.Size(121, 20);
+            this.remote_office_lbl.TabIndex = 7;
+            this.remote_office_lbl.Text = "Remote/On-site";
+            // 
             // findjob
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.remote_office_lbl);
+            this.Controls.Add(this.work_experience_lbl);
+            this.Controls.Add(this.job_title_lbl);
             this.Controls.Add(this.get);
             this.Controls.Add(this.jobs);
             this.Controls.Add(this.remote_or_office);
@@ -94,6 +130,7 @@ namespace scraping
             this.Text = "Find your job";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -104,6 +141,9 @@ namespace scraping
         private System.Windows.Forms.ComboBox remote_or_office;
         private System.Windows.Forms.RichTextBox jobs;
         private System.Windows.Forms.Button get;
+        private System.Windows.Forms.Label job_title_lbl;
+        private System.Windows.Forms.Label work_experience_lbl;
+        private System.Windows.Forms.Label remote_office_lbl;
     }
 }
 
